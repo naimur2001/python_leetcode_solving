1e4015ad333efbabd6566a9a3465e1dc13d7f16f
naimur2001 <naimurrahman18thapril@gmail.com>
2025-05-09 19:36:18 +0600

    done

--- Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.py
+++ Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.py
@@ -7,3 +7,16 @@
 # rotate 1 steps to the right: [7,1,2,3,4,5,6]
 # rotate 2 steps to the right: [6,7,1,2,3,4,5]
 # rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+class Solution:
+    def rotate(self, nums: list[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k= k%len(nums)
+        nums[:]=nums[-k:]+nums[:-k]
+        return nums
+
+  
+s1=Solution()
+print(s1.rotate([1,2,3,4,5,6,7],3))
